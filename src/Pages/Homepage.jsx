67bf263d5742630cf62f6c7d583d8f87e0fbeb38
RounakBhandari <bhandarirounak38@gmail.com>
2024-10-28 18:16:28 +0545

    initial commit

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,36 +1,10 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Homepage = () => {
-
-const [data, setData] = useState([]);
-  const getData = async () => {
-    try {
-     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-     setData(response.data) 
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-
-  useEffect(() => {
-    getData();
-    }
-  , []);
-  
-
-
   return (
     <div>
-      <div className="p-5">
-        {data.map((post, i)=>{
-          return <div>hello</div>
-})}
-      </div>
+      
     </div>
-    
-    
   )
 }
 
