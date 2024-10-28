c20b0052b3bd8e43a7c880b4736148a86b04df82
RounakBhandari <bhandarirounak38@gmail.com>
2024-10-28 18:15:03 +0545

    axioss final

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,21 +1,35 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Homepage = () => {
+
+const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+     setData(response.data) 
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  useEffect(() => {
+    getData();
+    }
+  , []);
   
-  axios.get('https://api.escuelajs.co/api/v1/products')
-  .then((val)=>{
-    console.log(val.data)
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
-
-
 
 
   return (
-    <div>Homepage</div>
+    <div>
+      <div className="p-5">
+        {data.map((post, i)=>{
+          return <div>hello</div>
+})}
+      </div>
+    </div>
+    
     
   )
 }
