2315d7a0301b98cebf0a672db0f20b3e5eb4452b
RounakBhandari <bhandarirounak38@gmail.com>
2024-11-22 13:00:35 +0545

    Final change

--- src/Pages/MealDescription.jsx
+++ src/Pages/MealDescription.jsx
@@ -1,24 +1,54 @@
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import { useApiHooks } from "../hooks/apiHooks"
+import { useState } from "react";
+import { Button } from "@material-tailwind/react";
 
 const MealDescription = () => {
+    const [play,setPlay]=useState(false);
+    const [count,setCount] =useState(0);
 const {id} = useParams()
     const {data,load,err} = useApiHooks('https://www.themealdb.com/api/json/v1/1/lookup.php',{
         i: id
     })
+const nav = useNavigate();
+    const handleClick=()=>{
+        setCount(count + 1);
+        if(count===0){
+            setPlay(true);
+        }
+        else{
+            setPlay(false);
+        }
+        
+    }
     return (
     <div className="p-4">
         {data && data.meals.map(({idMeal,strMeal,strInstructions,strYoutube})=>{
-           return <div className="flex justify-around gap-10 " key={idMeal}>
+           return <div className="block justify-around gap-10 p-4" key={idMeal}>
                 <div className="">
-                    <iframe width="420" height="315"
-                        src={`https://www.youtube.com/embed/${strYoutube.split('=')[1]}`}>
+                    <h1 className="font-bold text-3xl">{strMeal}</h1>
+                    <p className="mt-4 text-justify font-monospace">{strInstructions}</p>
+                    <div className="flex justify-center items-center mt-4">{count>=1?<Button className="m-4" ><a href={strYoutube} target="_blank">Watch On Youtube</a></Button>:
+                    <Button className="m-4" onClick={()=>handleClick()}>Watch Video</Button>}
+                    </div>
+                </div>
+                {
+                play &&<div className="relative w-full mt-2 mb-4 shadow-2xl "style={{paddingTop: "56.25%"}}>
+                    <iframe 
+                    src={`https://www.youtube.com/embed/${strYoutube.split('=')[1]}`}
+                    className="rounded shadow-lg absolute top-0 left-0 w-full h-full" 
+                    allowFullScreen
+                    >
                     </iframe>
                 </div>
-                <div className="">
-                    <h1>{strMeal}</h1>
-                    <p>{strInstructions}</p>
-                </div>
+                }
+                
+                    
+                
+        {
+
+        }
+
             </div>
 
         })}
