c9c7429fcc810349729b20473b2edfd632b0d215
RounakBhandari <bhandarirounak38@gmail.com>
2024-11-25 22:32:24 +0545

    Some validation done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {
   Navbar,
   Collapse,
-  Typography,
+  // Typography,
   IconButton,
 } from "@material-tailwind/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
@@ -43,12 +43,12 @@ export function NavBar() {
  
   return (
     <Navbar className="mx-auto max-w-screen-xl px-6 py-3">
-      <div className="flex items-center justify-between text-blue-gray-900">
+      <div className="flex items-center justify-center text-blue-gray-900">
         <NavLink
           to="/"
-          className="mr-4 cursor-pointer py-1.5 font-bold"
+          className="mr-4 cursor-pointer py-1.5 font-bold text-center"
         >
-          Rounak Bhandari
+          Form Validation ( formik and Yup )
         </NavLink>
         <div className="hidden lg:block">
           <NavList />
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,48 @@
-
-
+import { Button, Checkbox, Input, Option, Radio, Select, Textarea, Typography } from "@material-tailwind/react"
+import { useFormik} from "formik";
+import * as Yup from "yup";
 const Homepage = () => {
-  
+  const formSchema = Yup.object({
+    username: Yup.string().required().min(5).max(20)  ,
+    mail: Yup.string().required().matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,"Enter a valid mail")
+  })
+  const {values, errors , handleChange, handleSubmit,touched, setFieldValue} = useFormik({
+    initialValues:{
+        username: '',
+        mail: '',
+        gender: '',
+        hobby: [],
+        country: '',
+        message:''
+    },
+      onSubmit: (val)=>{
+        console.log(val);
+      },
+      validationSchema: formSchema,
+  })
 return (
-    <div className="text-center p-6 text-xl">
-      
+    <div className=" p-4 flex justify-center ">
+      <form className=" space-y-1 p-5 rounded-lg border-solid border-2 border-gray-400 " onSubmit={handleSubmit} >
+        <Input label="Username" name="username" onChange={handleChange}/> <p className="text-red-800">{touched.username && errors.username}</p>  
+        <Input label="Email" name="mail" onChange={handleChange} /> {touched.mail && errors.mail}
+        <Typography>Select your Gender</Typography>
+        <Radio label="Male" value={'Male'} name="gender" onChange={handleChange}/>
+        <Radio label="Female" value={'Female'} name="gender" onChange={handleChange} />
+        <Typography>Select your Hobbies</Typography>
+        <Checkbox label="Dance" value={'Dance'} name="hobby" onChange={handleChange}/>
+        <Checkbox label="Sing" value={'Sing'} name="hobby" onChange={handleChange}/>
+        <Checkbox label="Draw" value={'Draw'} name="hobby" onChange={handleChange}/>
+        <Checkbox label="Code" value={'Code'} name="hobby" onChange={handleChange}/>
+        <Select label="Select your Country" name="country" onChange={(e)=>setFieldValue('country',e)}>
+          <Option value="Nepal">Nepal</Option>
+          <Option value="India">India</Option>
+          <Option value="Pakistan">Pakistan</Option>
+          <Option value="China">China</Option>
+          <Option value="Japan">Japan</Option>
+        </Select>
+        <Textarea label="Enter your words..." value={values.message} name="message" onChange={handleChange} />
+        <Button size="sm" type="submit">Submit</Button>
+      </form>
     </div>
 )
 }
