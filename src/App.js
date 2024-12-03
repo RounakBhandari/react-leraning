946c0d988230df694728f97150846cb332c1939b
RounakBhandari <bhandarirounak38@gmail.com>
2024-12-03 16:57:10 +0545

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Homepage from "./Pages/Homepage";
-import RootLayout from "./Pages/RootLayout";
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <Homepage />,
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-};
-
-export default App;
+  return (
+    <div>App</div>
+  )
+}
+export default App
