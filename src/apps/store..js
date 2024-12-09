1b3c7cf8ab945eac2a6f14029bc0dd06dca23fa1
RounakBhandari <bhandarirounak38@gmail.com>
2024-12-09 08:59:01 +0545

    mistake reolved

--- src/apps/store..js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { cocktailApi } from "../features/cocktail/cocktailApi";
-
-export const store = configureStore({
-  reducer: {
-    [cocktailApi.reducerPath]: cocktailApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([cocktailApi.middleware]),
-});
