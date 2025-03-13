ac29675d5de70f518497735a716f8867bbe218ab
RounakBhandari <bhandarirounak38@gmail.com>
2025-03-13 18:52:39 +0545

    sdf

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -2,6 +2,6 @@ import { Spinner } from "@material-tailwind/react";
  
 export function Spinners() {
   return <div className="flex justify-center items-center h-screen">
-      <Spinner className="h-16 w-16 text-blue-800" />;
+      <Spinner className="h-16 w-16 text-blue-800" />
   </div>
 }
