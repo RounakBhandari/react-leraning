54d3f855e8235feaa54a8543124a25c77c30906c
RounakBhandari <bhandarirounak38@gmail.com>
2025-03-16 19:01:50 +0545

    initial

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Homepage from "./Pages/Homepage";
-import RootLayout from "./Pages/RootLayout";
-import AddForm from "./Pages/AddPage";
-import UpdateForm from "./Pages/UpdateForm";
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <Homepage />,
-        },
-        {
-          path: "addform",
-          element: <AddForm />,
-        },
-        {
-          path: "updateform/:id",
-          element: <UpdateForm />,
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+	return <div>
+    
+  </div>;
 };
 
 export default App;
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "./userSlice";
-
-//configures a store which will be wrapped around the app in index.js file so that each component and pages will have access to this store.
-export const store = configureStore({
-  reducer: {
-    userSlice: userSlice.reducer,
-  },
-});
